8c0f1377811807a61eaaa7706f3ca18392daf570
miazigoo <kasper6024@gmail.com>
2025-04-05 21:41:08 +0300

    Добавлен PostgreSQL вместо SQLite Запуск приложения в docker контейнерах

--- database.py
+++ database.py
@@ -1,13 +1,23 @@
+from environs import Env
 from sqlalchemy import  create_engine
 from sqlalchemy.orm import sessionmaker
 
 from secret_api.models import Base
 
-SQL_DB_URL = "sqlite:///./my.db"
+
+env = Env()
+env.read_env()
+
+POSTGRES_USER = env.str("POSTGRES_USER")
+POSTGRES_PASSWORD = env.str("POSTGRES_PASSWORD")
+POSTGRES_HOST = env.str("POSTGRES_HOST")
+POSTGRES_PORT = env.int("POSTGRES_PORT")
+POSTGRES_DB = env.str("POSTGRES_DB")
+
+POSTGRESQL_URL = f"postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+
 engine = create_engine(
-    SQL_DB_URL,
-    connect_args={"check_same_thread": False}
-)
+    POSTGRESQL_URL)
 
 new_session = sessionmaker(autoflush=False, autocommit=False, bind=engine)
 
